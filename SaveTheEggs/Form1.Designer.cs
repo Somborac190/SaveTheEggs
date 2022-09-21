b0a842a9530794fb9af1bcfaa37e6426e4a426d6
Somborac190 <radosmirkovic@yahoo.com>
2022-09-21 19:11:40 +0200

    my first commit

---
+++ SaveTheEggs/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace SaveTheEggs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.txtSaved = new System.Windows.Forms.Label();
+            this.txtMissed = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::SaveTheEggs.Properties.Resources.egg;
+            this.pictureBox1.Location = new System.Drawing.Point(62, 70);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(40, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Tag = "eggs";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(265, 70);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(40, 50);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Tag = "eggs";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(433, 70);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(40, 50);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 0;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Tag = "eggs";
+            // 
+            // player
+            // 
+            this.player.Image = global::SaveTheEggs.Properties.Resources.chicken_normal;
+            this.player.Location = new System.Drawing.Point(253, 569);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(85, 80);
+            this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            // 
+            // txtSaved
+            // 
+            this.txtSaved.AutoSize = true;
+            this.txtSaved.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSaved.Location = new System.Drawing.Point(12, 9);
+            this.txtSaved.Name = "txtSaved";
+            this.txtSaved.Size = new System.Drawing.Size(85, 24);
+            this.txtSaved.TabIndex = 1;
+            this.txtSaved.Text = "Saved:0";
+            // 
+            // txtMissed
+            // 
+            this.txtMissed.AutoSize = true;
+            this.txtMissed.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMissed.Location = new System.Drawing.Point(520, 9);
+            this.txtMissed.Name = "txtMissed";
+            this.txtMissed.Size = new System.Drawing.Size(93, 24);
+            this.txtMissed.TabIndex = 1;
+            this.txtMissed.Text = "Missed:0";
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.MainGameTimerEvent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(614, 661);
+            this.Controls.Add(this.txtMissed);
+            this.Controls.Add(this.txtSaved);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Tag = "eggs";
+            this.Text = "Save the Eggs";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyIsUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.Label txtSaved;
+        private System.Windows.Forms.Label txtMissed;
+        private System.Windows.Forms.Timer gameTimer;
+    }
+}
+
---
+++ SaveTheEggs/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SaveTheEggs
+{
+    public partial class Form1 : Form
+    {
+        bool goleft, goRight;
+        int speed = 8;
+        int missed = 0;
+        int score = 0;
+
+        Random randX = new Random();
+        Random randY = new Random();
+
+        PictureBox splash = new PictureBox();
+
+        public Form1()
+        {
+            InitializeComponent();
+            restartGame();
+        }
+
+        private void MainGameTimerEvent(object sender, EventArgs e)
+        {
+            txtSaved.Text = "Saved:" + score;
+            txtMissed.Text = "Missed:" + missed;
+
+            if(goleft == true && player.Left > 0)
+            {
+                player.Left -= 12;
+                player.Image = Properties.Resources.chicken_normal2;
+            }
+            if(goRight == true && player.Left + player.Width < this.ClientSize.Width)
+            {
+                player.Left += 12;
+                player.Image = Properties.Resources.chicken_normal;
+            }
+
+            foreach(Control x in this.Controls)
+            {
+                if(x is PictureBox && (string)x.Tag == "eggs")
+                {
+                    x.Top += speed;
+
+                    if(x.Top + x.Height > this.ClientSize.Height)
+                    {
+                        splash.Image = Properties.Resources.splash;
+                        splash.Location = x.Location;
+                        splash.Height = 60;
+                        splash.Width = 60;
+                        splash.BackColor = Color.Transparent;
+
+                        this.Controls.Add(splash);
+
+                        x.Top = randY.Next(80, 300) * -1;
+                        x.Left = randX.Next(5, this.ClientSize.Width - x.Width);
+                        missed += 1;
+                        player.Image = Properties.Resources.chicken_hurt;
+                    }
+
+                    if (player.Bounds.IntersectsWith(x.Bounds))
+                    {
+                        x.Top = randY.Next(80, 300) * -1;
+                        x.Left = randX.Next(5, this.ClientSize.Width - x.Width);
+                        score += 1;
+                    }
+                }
+            }
+
+            if(score > 10)
+            {
+                speed = 12;
+            }
+            if(missed > 5)
+            {
+                gameTimer.Stop();
+                MessageBox.Show("Game over" + Environment.NewLine + "You lost good eggs" + Environment.NewLine + "Click ok to restart");
+                restartGame();
+            }
+
+
+        }
+
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Left)
+            {
+                goleft = true;
+            }
+            if(e.KeyCode == Keys.Right)
+            {
+                goRight = true;
+            }
+        }
+
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                goleft = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = false;
+            }
+        }
+
+        private void restartGame()
+        {
+            foreach(Control x in this.Controls)
+            {
+                if(x is PictureBox && (string)x.Tag == "eggs")
+                {
+                    x.Top = randY.Next(80, 300) * -1;
+                    x.Left = randX.Next(5, this.ClientSize.Width - x.Width);
+                }
+            }
+            player.Left = this.ClientSize.Width / 2;
+            player.Image = Properties.Resources.chicken_normal;
+
+            score = 0;
+            missed = 0;
+            speed = 8;
+
+            goleft = false;
+            goRight = false;
+            gameTimer.Start();
+            
+        }
+    }
+}
